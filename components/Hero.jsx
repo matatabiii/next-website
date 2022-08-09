0af7ec6a99b8581c6c413cfbd92cc1ed41369fac
KeishirouKawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-08-09 21:13:37 +0900

    共通コンポーネント作成

---
+++ components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div>
+      <h1>BLOG</h1>
+      <p>Next.js × ReactでWebサイト構築</p>
+    </div>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/header";
+import { Hero } from "../components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
+    </>
+  )
+}
